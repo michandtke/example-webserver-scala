e7ce229ce64e2d01e83f223ee268d75772305f6e
Michael Wandtke <Michael.Wandtke@gmail.com>
2021-05-10 15:25:22 +0200

    Add test for put, use Accessor for getting the current value

--- src/main/scala/Accessor.scala
+++ src/main/scala/Accessor.scala
@@ -1,7 +1,7 @@
 import scala.concurrent.Future
 
 object Accessor {
-  def create(storage: Storage, value: ToDo): Future[Unit] = {
-    storage.put(value)
-  }
+  def create(storage: Storage, value: ToDo): Future[Unit] = storage.put(value)
+
+  def get(storage: Storage, id: Int): Future[ToDo] = storage.get(id)
 }
--- src/test/scala/PutToDoTest.scala
+++ src/test/scala/PutToDoTest.scala
@@ -13,9 +13,25 @@ class PutToDoTest extends AnyFlatSpec with should.Matchers {
 
     // when
     Await.ready(Accessor.create(storage, todo), 1.second)
-    val id0: ToDo = Await.result(storage.get(id), 1.second)
+    val id0: ToDo = Await.result(Accessor.get(storage, id), 1.second)
 
     // then
     id0 should be (todo)
+  }
+
+  "A previous ToDo" should "be replaced" in {
+    // given
+    val id = 0
+    val todo1 = ToDo(id = id, name = "name", description = "desc", done = false)
+    val todo2 = ToDo(id = id, name = "name", description = "desc", done = true)
+    val storage = new TestStorage()
+    Await.ready(Accessor.create(storage, todo1), 1.second)
+
+    // when
+    Await.ready(Accessor.create(storage, todo2), 1.second)
+    val result = Await.result(Accessor.get(storage, id), 1.second)
+
+    // then
+    result should be (todo2)
   }
 }
